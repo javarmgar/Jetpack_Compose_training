16f8a40d314f82ead762fea774e04aca0393f420
Javier Armenta <j.armentagarcia@fetchrewards.com>
2023-10-09 16:12:51 -0600

    Next approach can be to use a new API State/MutableSate Whenever the data changes it will trigger updates to the UI to recompose the UI but... -> declaration within a composable function will recreate with initial value everytime it is recomposed
    Solution:
    Use an API wrapper called Remember that will holds and remember the State variable, so that there are two possible scenarios:
     1.- first time -  creation : it will create the variable and assign the value to the reference
     2- subsequent times - It will look for any value stored and will assign this value to the reference

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -13,6 +13,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -67,13 +70,17 @@ Next approach can be to use a new API State/MutableSate
 Whenever the data changes it will trigger updates to the UI to recompose the UI
 but... -> declaration within a composable function will recreate with initial value everytime it is recomposed
 
+Solution:
+Use an API wrapper called Remember that will holds and remember the State variable, so that there are two possible scenarios:
+ 1.- first time -  creation : it will create the variable and assign the value to the reference
+ 2- subsequent times - It will look for any value stored and will assign this value to the reference
 */
 
 
 @Composable
 
 fun Greeting(name: String) {
-    var expanded = false
+    var expanded: MutableState<Boolean> = remember{ mutableStateOf(false) }
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
@@ -82,8 +89,8 @@ fun Greeting(name: String) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
-            ElevatedButton(onClick = { expanded = !expanded }) {
-                Text(text = if (expanded) "Show less" else "Show more")
+            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(text = if (expanded.value) "Show less" else "Show more")
                 /*
                 The reason why mutating this variable does not trigger recompositions is that it's not being tracked by
                 compose. Also, each time greeting is called the variable will reset to false
