ca6ebe02cdf3a9bcd8c2d61f9b6cf0ae4d1acd22
Javier Armenta <j.armentagarcia@fetchrewards.com>
2023-10-09 17:12:53 -0600

    new attrs     verticalArrangement: Arrangement - values top, bottom, center     horizontalAlignment: Alignment - values start. end. centerHorizontally     by reserved keyword: kotlin feature to apply delefate pattern, it unwraps the MutableState(boolean) into

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,18 +3,23 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -85,7 +90,9 @@ fun Greeting(name: String) {
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
@@ -106,5 +113,44 @@ fun Greeting(name: String) {
 fun GreetingPreview() {
     BasicsCodelabTheme {
         MyApp()
+    }
+}
+
+
+/*
+ We can have multiple previews at the same time
+
+Column attrs
+    verticalArrangement: Arrangement - values top, bottom, center
+    horizontalAlignment: Alignment - values start. end. centerHorizontally
+    by reserved keyword: kotlin feature to apply delefate pattern, it unwraps the MutableState(boolean) into
+    boolean it saves us from typing .value every time
+    shouldShowOnboarding is set to false but still pending the data source
+ */
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier){
+    var shouldShowOnboarding: Boolean by remember { mutableStateOf(true) }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = "Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = { shouldShowOnboarding = false}) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+/*
+Fixed height to verify content is aligned as stated
+ */
+@Preview(showBackground = true, widthDp = 320, heightDp = 320 )
+@Composable
+fun OnboardingPreview(){
+    BasicsCodelabTheme {
+        OnboardingScreen()
     }
 }
