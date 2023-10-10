6e6bb0b77c61ea45dd125879d328b6a9cc791949
Javier Armenta <j.armentagarcia@fetchrewards.com>
2023-10-09 20:04:15 -0600

    Animation API animattionDpAsState     It is a high level API composable, it returns a state object whose value will continuously be updated     by the animation until it finishes. It takes a Dp target value.

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -26,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
+import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -150,7 +152,7 @@ Compose also looks at what data is needed by an individual composable so that it
 
 
 Composable functions can execute frequently and in any order, you must not rely on thee ordering in
- which the code is exectued, or on how many times this function will be recomposed.
+ which the code is executed, or on how many times this function will be recomposed.
 
 Here we don't have an state so this won't work as expected
 
@@ -178,8 +180,14 @@ activity recreation ( configuration changes and process death)
  */
 @Composable
 fun Greeting(name: String) {
-    val expanded: MutableState<Boolean> = rememberSaveable{ mutableStateOf(false) }
-    val extraPadding = if(expanded.value) 48.dp else 0.dp
+    var expanded by rememberSaveable{ mutableStateOf(false) }
+    /*
+    Animation API animattionDpAsState
+    It is a high level API composable, it returns a state object whose value will continuously be updated
+    by the animation until it finishes. It takes a Dp target value.
+     */
+    val extraPadding by animateDpAsState( if (expanded) 48.dp else 0.dp, label = "")
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
@@ -190,8 +198,8 @@ fun Greeting(name: String) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
-            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(text = if (expanded.value) "Show less" else "Show more")
+            ElevatedButton(onClick = { expanded = !expanded }) {
+                Text(text = if (expanded) "Show less" else "Show more")
                 /*
                 The reason why mutating this variable does not trigger recompositions is that it's not being tracked by
                 compose. Also, each time greeting is called the variable will reset to false
